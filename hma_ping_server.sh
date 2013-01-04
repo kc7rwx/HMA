5cf44409a067eca87800a7492013e749b0aaca8b
Robert Anderson <kc7rx@gmail.com>
2013-01-04 15:27:53 +0900

    some updates

--- hma_ping_server.sh
+++ hma_ping_server.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 cat hma_server_list.csv | grep ,$1, | while read LINE
 do
-  IP=$(echo $LINE | awk -F, '{print $1}');
-  PING_TIME=$(ping $IP -c3|grep rtt);
-  OUTPUT=$LINE$PING_TIME;
-  echo $OUTPUT;
+  SERVER=$LINE;
+  IP=$(echo $LINE|awk -F, '{print $1}');
+  PING=$(ping $IP -c3|grep round-trip|awk '{print $4}'|awk -F/ '{print $1","$2","$3}');
+  echo $SERVER,$PING;
+ # echo $OUTPUT;
 done
 
